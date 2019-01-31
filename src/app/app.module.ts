d12be4dbb52e53bdd9abf1ff3ff01ac6a57704d4
Samruddhi Ganvir <Devender Shekhawats>
2019-01-31 11:00:09 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './banner/banner.component';
+import { MatchesComponent } from './matches/matches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BannerComponent,
+    MatchesComponent
   ],
   imports: [
     BrowserModule,
